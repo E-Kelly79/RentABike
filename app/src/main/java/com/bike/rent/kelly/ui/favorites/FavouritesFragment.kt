cc23f7770f3608769bf8a57b3d9b228490484f32
E-Kelly79 <k.eoin@yahoo.ie>
2019-02-22 18:46:37 +0000

    Added new image for landing page

--- app/src/main/java/com/bike/rent/kelly/ui/favorites/FavouritesFragment.kt
+++ app/src/main/java/com/bike/rent/kelly/ui/favorites/FavouritesFragment.kt
@@ -1,22 +1,28 @@
 package com.bike.rent.kelly.ui.favorites
 
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import com.bike.rent.kelly.R
 import com.bike.rent.kelly.model.favs.Favourites
 import com.bike.rent.kelly.ui.base.BaseFragment
-
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.marcoscg.dialogsheet.DialogSheet
 import kotlinx.android.synthetic.main.favourites_fragment.mFavRecyclerView
+import kotlinx.android.synthetic.main.prgress_dialog.*
 import timber.log.Timber
 
 class FavouritesFragment: BaseFragment() {
@@ -67,8 +73,23 @@ class FavouritesFragment: BaseFragment() {
         })
     }
 
+    @SuppressLint("ResourceType")
     fun deleteFavourite(favId: String) {
         val drFavourite = FirebaseDatabase.getInstance().getReference("Favourites").child(favId)
-        drFavourite.removeValue()
+            val dialogSheet: DialogSheet = DialogSheet(context)
+            dialogSheet.setTitle(R.string.dialog_warning)
+                .setMessage(R.string.dialog_delete)
+                .setCancelable(true)
+                .setPositiveButton(R.string.dialog_delete_btn){
+                    drFavourite.removeValue()
+                }
+                .setNegativeButton("Cancel"){
+
+                }
+                .setRoundedCorners(false)
+                .setBackgroundColor(Color.WHITE)
+                .setButtonsColorRes(R.color.color_primary)
+                .show()
+
     }
 }
--- app/src/main/java/com/bike/rent/kelly/utils/ProgressDialog.kt
+++ app/src/main/java/com/bike/rent/kelly/utils/ProgressDialog.kt
@@ -4,16 +4,51 @@ import android.app.Dialog
 import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.support.v4.content.res.ResourcesCompat.getColor
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import com.bike.rent.kelly.R
 
 class ProgressDialog {
+
     companion object {
+        var title: TextView? = null
+        var loading: TextView? = null
+        var body: TextView? = null
+        var footerLink: TextView? = null
+        var progress: ProgressBar? = null
+        var btn: Button? = null
         fun progressDialog(context: Context): Dialog {
+
             val dialog = Dialog(context)
             val inflate = LayoutInflater.from(context).inflate(R.layout.prgress_dialog, null)
             dialog.setContentView(inflate)
             dialog.setCancelable(false)
+            loading!!.visibility = View.VISIBLE
+            progress!!.visibility = View.VISIBLE
+            dialog.window!!.setBackgroundDrawable(
+                ColorDrawable(Color.TRANSPARENT)
+            )
+            return dialog
+        }
+
+        fun deleteDialog(context: Context): Dialog {
+            val dialog = Dialog(context)
+            val inflate = LayoutInflater.from(context).inflate(R.layout.prgress_dialog, null)
+            dialog.setContentView(inflate)
+            dialog.setCancelable(true)
+            footerLink!!.text = context.getString(R.string.dilaog_close)
+            btn!!.text = context.getString(R.string.dialog_delete_btn)
+            btn!!.setBackgroundColor(context.resources.getColor(android.R.color.holo_red_light))
+            btn!!.setTextColor(context.resources.getColor(R.color.color_white))
+            body!!.text = context.getString(R.string.dialog_delete)
+            title!!.visibility = View.VISIBLE
+            body!!.visibility = View.VISIBLE
+            btn!!.visibility = View.VISIBLE
+            footerLink!!.visibility = View.VISIBLE
             dialog.window!!.setBackgroundDrawable(
                 ColorDrawable(Color.TRANSPARENT)
             )
