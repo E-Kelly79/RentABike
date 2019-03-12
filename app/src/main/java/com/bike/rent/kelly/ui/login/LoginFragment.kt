57509cc9abf2ccfa1f4fc50edf178cfc33824599
E-Kelly79 <k.eoin@yahoo.ie>
2019-03-12 15:31:34 +0000

    Google sign implemented

--- app/src/main/java/com/bike/rent/kelly/ui/login/LoginFragment.kt
+++ app/src/main/java/com/bike/rent/kelly/ui/login/LoginFragment.kt
@@ -1,35 +1,62 @@
 package com.bike.rent.kelly.ui.login
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import com.bike.rent.kelly.R
 import com.bike.rent.kelly.ui.base.BaseFragment
 import com.bike.rent.kelly.utils.SnackBars
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInResult
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.common.api.GoogleApiClient.Builder
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.mapbox.core.utils.TextUtils
 import kotlinx.android.synthetic.main.login_fragment.*
-import kotlinx.android.synthetic.main.map_fragment.mCitySpinner
-import kotlinx.android.synthetic.main.map_fragment.mLandingBtn
+import timber.log.Timber
 
-class LoginFragment: BaseFragment(){
+class LoginFragment: BaseFragment(), GoogleApiClient.OnConnectionFailedListener{
 
     lateinit var mView: View
     var mAuth: FirebaseAuth? = null
     var mDatabase: DatabaseReference? = null
-    lateinit var contractName: Bundle
+
+    val RC_SIGN_IN: Int = 9001
+    private var mGoogleApiClient: GoogleApiClient? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mAuth = FirebaseAuth.getInstance()
+        //updateUi(false)
+        val gso: GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        mGoogleApiClient = GoogleApiClient.Builder(context!!)
+            .enableAutoManage(baseActivity, this)
+            .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+            .build()
+
+        //mGoogleSigninClient = GoogleSignIn.getClient(context!!, mGoogleSignin)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+
+    }
+
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {
+        Timber.d("onConnectionFailed: " + connectionResult)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -42,6 +69,11 @@ class LoginFragment: BaseFragment(){
             View.SYSTEM_UI_LAYOUT_FLAGS
         )
 
+        googleSigninBtn.setOnClickListener {
+            var signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+
         mLoginBtn.setOnClickListener {
             var email = mLoginEmail.text.toString().trim()
             var password = mLoginPassword.text.toString().trim()
@@ -53,6 +85,25 @@ class LoginFragment: BaseFragment(){
             }
         }
     }
+
+
+    private fun signOut(){
+
+    }
+
+    private fun handleResult(result: GoogleSignInResult){
+
+    }
+
+
+//    private fun updateUi(isLoggedIn: Boolean){
+//        if(isLoggedIn){
+//            googleSigninLayout.visibility = View.VISIBLE
+//        }else{
+//            googleSigninLayout.visibility = View.GONE
+//        }
+//
+//    }
 
     private fun loginUser(email: String, password: String) {
         mAuth!!.signInWithEmailAndPassword(email, password)
